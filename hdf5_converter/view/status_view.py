e2c5c75415294e2fa3b7f5633469542d581ffac8
Christofanis Skordas <55315750+skordaschristofanis@users.noreply.github.com>
2025-04-18 11:06:12 -0500

    adds update status signal, line nubmers and clear method

--- hdf5_converter/view/status_view.py
+++ hdf5_converter/view/status_view.py
@@ -28,19 +28,34 @@
 
 from gsewidgets import TextInfoBox, HorizontalLine
 from qtpy.QtWidgets import QFrame, QVBoxLayout
+from qtpy.QtCore import QObject, Signal
 
 
 class StatusView(QFrame):
     """Creates the status view of the HDF5 Converter GUI."""
+
+    update_status = Signal(str)
     
     def __init__(self) -> None:
         """Initialises the status view."""
         super(StatusView, self).__init__()
 
         self.txt_info = TextInfoBox()
+        self._line_number = 0
 
         # Set the layout of the status view
+        self.update_status.connect(self._add_status_message)
         self._layout()
+
+    def _add_status_message(self, message: str) -> None:
+        """Adds a status message to the status view."""
+        self._line_number += 1
+        self.txt_info.append(f"{self._line_number}. {message}")
+
+    def clear(self) -> None:
+        """Clears the status view."""
+        self.txt_info.clear()
+        self._line_number = 0
 
     def _layout(self) -> None:
         """Sets the layout of the status view."""
